329f5e36667e858c790755d05e9f4dbc2ffede86
Dev-Abdalla-Magdy <you@example.com>
2023-08-13 03:05:07 +0300

    Uploading Files

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/* Start Scroller */
+
+let scroll = document.querySelector(".scroller");
+let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll", () => {
+  let scrollTop = document.documentElement.scrollTop;
+  scroll.style.width = `${((scrollTop / height) * 100)}%`;
+})
+
+/* End Scroller */
+
+/* Start Other Links OnClick */
+const links = document.querySelector(".links")
+const menu = document.querySelector("#menu")
+
+menu.addEventListener("click", () => {
+  links.classList.toggle("active")
+  menu.classList.toggle("active")
+})
+/* End Other Links OnClick */
