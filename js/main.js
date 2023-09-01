09d44d9e8c9fb83b05e50e60aa28a8e6af197fcf
Dev-Abdalla-Magdy <you@example.com>
2023-09-01 04:56:00 +0300

    First Version Has Finished

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 /* Start Scroller */
 
 let scroll = document.querySelector(".scroller");
-let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+let height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
 window.addEventListener("scroll", () => {
   let scrollTop = document.documentElement.scrollTop;
-  scroll.style.width = `${((scrollTop / height) * 100)}%`;
-})
+  scroll.style.width = `${(scrollTop / height) * 100}%`;
+});
 
 /* End Scroller */
 
 /* Start Other Links OnClick */
-const links = document.querySelector(".links")
-const menu = document.querySelector("#menu")
+const links = document.querySelector(".links");
+const menu = document.querySelector("#menu");
 
 menu.addEventListener("click", () => {
-  links.classList.toggle("active")
-  menu.classList.toggle("active")
-})
+  links.classList.toggle("active");
+  menu.classList.toggle("active");
+});
 /* End Other Links OnClick */
